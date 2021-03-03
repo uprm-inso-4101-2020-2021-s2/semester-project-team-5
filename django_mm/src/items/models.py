739ebf09a3a4afcd07e9e68b886596623a0842ee
xstephaniex <stephanie.vargas@upr.edu>
2021-03-02 20:37:06 -0400

    Connected list view with the detail view and fixed urls.py for Items

--- django_mm/src/items/models.py
+++ django_mm/src/items/models.py
@@ -43,8 +43,13 @@ class Item(models.Model):
     quantity = models.IntegerField(default=1, null=False)
     Category = models.CharField(max_length=20, null=False, default='', unique=True)
 
-
     objects = ItemManager()
+
+
+    def get_absolute_url(self):
+        return "/items/{Category}/".format(Category=self.Category)
+
+
 
     def __str__(self):
         return self.name
