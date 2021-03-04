ab480a05232cb78ef6646c8e9cb66ac66bac2173
Waldemar Reyes <waldemar.reyes@upr.edu>
2021-03-04 03:07:04 -0400

    Added Items to home_page content

--- django_mm/src/ecommerce/views.py
+++ django_mm/src/ecommerce/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from items.models import Item
 
 
 def home_page(request):
+    items = Item.objects.all()
     context = {
         "members": "STEPH WALLY MANNY HEC CORA ANGEL",
         "content": "Best Puerto rican ecommerce site",
+        "items": items
 
     }
     if request.user.is_authenticated:
