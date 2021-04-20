e01458343f1fccba856b77f53a1e650c7a7f1cfe
xstephaniex <stephanie.vargas@upr.edu>
2021-04-20 06:34:26 -0400

    fixing prod

--- ecommerce/settings/production.py
+++ ecommerce/settings/production.py
@@ -70,7 +70,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'html_templates'),
-                 os.path.join(BASE_DIR, 'html_templates/Items')],
+                 os.path.join(BASE_DIR, 'html_templates/items')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
