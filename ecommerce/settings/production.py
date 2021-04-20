9eff3d90cfd2356397f16041c3f420c071a2a372
xstephaniex <stephanie.vargas@upr.edu>
2021-04-20 06:47:47 -0400

    added items to templates

--- ecommerce/settings/production.py
+++ ecommerce/settings/production.py
@@ -69,7 +69,12 @@ ROOT_URLCONF = 'ecommerce.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'html_templates')],
+        'DIRS': [# os.path.join(BASE_DIR, 'html_templates'),
+                 os.path.join(BASE_DIR, 'Items', 'html_templates')
+
+
+
+                 ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
